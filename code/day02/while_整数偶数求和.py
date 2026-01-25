ff1f8740795a10a677c8577b2a2b14ac86e7aed7
RyanLrj <ryanlyu1223@gmail.com>
2026-01-25 15:42:09 +0100

    Add files via upload

---
+++ code/day02/while_整数偶数求和.py
@@ -0,0 +1,22 @@
+"""
+while循环0到100的整数与偶数求和
+
+version: 1.0
+author: Ryan
+"""
+
+total = 0
+i = 1
+while i <= 100:
+    total += i
+    i += 1
+print(total)
+
+#偶数求和
+
+totaltwo = 0
+i = 2
+while i <= 100:
+    totaltwo += i
+    i += 2
+print(totaltwo)
