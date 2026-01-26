4012562df7a112f99ceab58cb0f0f73177436ec9
Ruijie Lyu <ryanlyu1223@gmail.com>
2026-01-26 17:09:19 +0100

    Add files via upload

--- code/day03/break_continue_example.py
+++ code/day03/break_continue_example.py
@@ -12,3 +12,13 @@ while True:
     if i > 100:
         break
 print(total)
+
+#引入continue
+
+total = 0
+i = 2
+for i in range (1,101):
+    if i % 2 == 0:
+        continue
+    total += i
+print(total)
