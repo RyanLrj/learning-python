163d8e52d1c9b67ff4d7b7bc65777bedb2a3b974
Ruijie Lyu <ryanlyu1223@gmail.com>
2026-02-11 18:02:04 +0100

    Add files via upload

---
+++ code/day05/列表运算.py
@@ -0,0 +1,15 @@
+"""
+列表运算示例
+
+version: 1.0
+author: Ryan
+"""
+
+items1 = [1,2,3,4,5]
+items2 = [6,7,8,9,10]
+items3 = ['a','b','c','d','e']
+print(items1 + items2)
+print(items1 + items3)
+items1 += items2
+print(items1)
+print(items2 * 3)
