41daf221ea5bcec79729b186e122e73574c6e2a3
Ruijie Lyu <ryanlyu1223@gmail.com>
2026-02-19 11:44:41 +0100

    Add files via upload

---
+++ code/day08/元素的位置与频次.py
@@ -0,0 +1,6 @@
+items = ['python','java','java','c++','kotlin','python']
+print(items.index('python'))
+print(items.index('python',1))
+print(items.count('python'))
+print(items.count('kotlin'))
+print(items.count('hello'))
